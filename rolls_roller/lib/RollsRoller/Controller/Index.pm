9cd2261372ae6774d0ca60bc52bb774aa9e2ab4f
Pavel <pkuptsov@gmail.com>
2015-03-10 16:53:47 +0300

    Debugging cookies

--- rolls_roller/lib/RollsRoller/Controller/Index.pm
+++ rolls_roller/lib/RollsRoller/Controller/Index.pm
@@ -12,16 +12,18 @@ sub welcome {
 # обрабатываем форму с заказом
 sub diller {
   my $self = shift;
-  my $param = $self->req->cookies;
-  print $param->printer,"\n";
+  #my $c = ;
+  #my $cookie = Mojo::Cookie::Request->parse($c);
+  #print Dumper $self->req->content->headers->cookie;
   # извлечем в хэш данные и обработаем их
-  #my $printer = _cooke_data_extract($param);  
-  $self->render(text => 'Good');
+  my $printer = _cooke_data_extract($self->req->content->headers->cookie);  
+  $self->render(text => 'Good: ' . $self->param('email'));
 }
 
 
 ## SUBS ##
 sub _cookie_data_extract {
+  #'sign1=on; light1=on; roll1=on; height1=on; roll2=on; height2=on; sign2=on; printer=regular'
   my $c = shift;
   my %data;
   #$data{$c->printer} = 
