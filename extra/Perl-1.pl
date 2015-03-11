f9da950a2e2447cd2e8e922f83de9f33534d6979
Pavel <pkuptsov@gmail.com>
2015-03-11 13:11:17 +0300

    Cookie extract sub

--- extra/Perl-1.pl
+++ extra/Perl-1.pl
@@ -1,26 +1,31 @@
 #!/usr/bin/perl -w
 use strict;
 
-
+my %printer = (
+               mat1  => 'on', 
+               sign1 => 'off',
+               roll1 => 'off',
+               light1 => 'off',
+               height1 => 'off'
+               );
 #'sign1=on; light1=on; roll1=on; height1=on; roll2=on; height2=on; sign2=on; printer=regular'
-  my ($printer,$c) = ('regular','sign_reqular=on; light_reqular=on; roll_reqular=on; height_reqular=on; roll_entry=on; height_entry=on; sign_entry=off;sign_premium=on printer=regular');
-  my (%data,@array_on);
+  my $c = 'sign1=on; light1=off; roll1=off; height1=on; roll2=on; height2=on; sign2=off;sign3=on printer=regular';
   chomp($c);
   $c =~ s/\s+//g;
   my @line = (split /;/,$c);
   foreach my $var(@line) {
     next if $var =~ /off$/i; #если выключена - пропускаем
-    print $var,"\n";
+    
+    #print $var,"\n";
     my ($name,$value) = split(/=/,$var,2);
-    print "!$name!\n";
-    print "!sign_$printer!\n";
-    next if $name eq "sign_$printer"; 
-    # если это не имя принтера и опция включена - добавляем в массив имя куки
-    if ($name =~ /[a-z]+_/g) {
-      push(@array_on,$name);
+    if (exists $printer{$name}) {
+        $printer{$name} = $value;
+        #print "$name: $value\n";
+    } else {
+        #print "$name: $printer{$name}\n";
     }
+
   }
   
-  $data{$printer} = @array_on;
+  print %printer;
   
-  print %data;
--- rolls_roller/lib/RollsRoller/Controller/Index.pm
+++ rolls_roller/lib/RollsRoller/Controller/Index.pm
@@ -13,35 +13,28 @@ sub welcome {
 sub diller {
   my $self = shift;
   #print Dumper $self->req->content->headers->cookie;
+  my $default_printer_config = $self->cookie('printer'); # TODO вынести в конфиг и получить натсройки в \хэш
   # извлечем в хэш данные и обработаем их
-  my $printer = _cookie_data_extract($self->cookie('printer'),$self->req->content->headers->cookie);
-  print Dumper($printer);
+  my $printer = _cookie_data_extract($self->req->content->headers->cookie);
   $self->render(text => 'Good: ' . $self->param('email'));
 }
-
 
 ## SUBS ##
 sub _cookie_data_extract {
   #'sign1=on; light1=on; roll1=on; height1=on; roll2=on; height2=on; sign2=on; printer=regular'
-  my ($printer,$c) = @_;
-  my (%data,@array_on);
+  my $c = shift;
+  my %printer;# хэш получаем по имени принтера из БД или конфига (сделать позже)
   chomp($c);
   $c =~ s/\s+//g;
   my @line = (split /;/,$c);
   foreach my $var(@line) {
-    next if $var =~ /off$/i;
-    say $var;
+    next if $var =~ /off$/i; #если выключена - пропускаем
     my ($name,$value) = split(/=/,$var,2);
-    # если это не имя принтера и опция включена - добавляем в массив имя куки
-    if ($name ne 'printer' and $value eq 'on') {
-      push(@array_on,$name);
+    if (exists $printer{$name}) {# отбираем только те значения что есть в хэше
+        $printer{$name} = $value;
     }
-    
-    
   }
-  
-  $data{$printer} = @array_on;
-  return \%data;
+  return \%printer;# возвращаем конфиг принтера сделанного клиентом
 }
 
 
